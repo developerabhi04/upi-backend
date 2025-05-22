fb17a390bf560c191321764da3cee1748f9ca170
Kumar <115116493+developerabhi04@users.noreply.github.com>
2025-05-22 15:11:12 +0530

    vv

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ app.use(
   })
 );
 
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+
 // Test Route
 app.get("/", (req, res) => {
   res.json({ success: true, message: "API is working!" });
